dc7d5a19d5c5e6932d4709db9968ef91c327e9f1
drummermanrob20 <robert@stapleton-family.net>
2022-08-10 13:56:24 -0400

    cleaned up for branch merge

--- GPO.Finder.ps1
+++ GPO.Finder.ps1
@@ -1,22 +1,5 @@
 <#
-$GPO = Get-GPO -All
-foreach ($g in $GPO) {
-    Get-GPOReport -Guid $g.Id.Guid -ReportType XML
-    foreach ($s in $Setting) {
-        see if the xml path to the desired settings exists
-        if (it exists) {
-            create a custom obj with the GPO Name, Guid, and Settings controlling it
-            (also needs to include whether or not the gpo is linked and enabled)
-            return the obj
-        }
-        if it doesn't exist, continue to the next item in the loop
-    }
-}
-
-- default output should be only GPOs enabled and linked
-    - param for -IncludeDisabled
-    - param for -IncludeUnlinked
-
+    - add in more settings to check
 #>
 param (
     [switch]$IncludeDisabled
@@ -28,10 +11,12 @@ $GPO = Get-GPO -All
 foreach ($g in $GPO) {
     [xml]$Report = Get-GPOReport -Guid $g.Id.Guid -ReportType 'XML'
     
+    # skip the GPO if disabled and user has not specified to include disabled
     if (
         $Report.GPO.LinksTo.Enabled -ne 'true' -and
         !$IncludeDisabled
         ) { Continue }
+
     # Folder Redirection
     if ($Report.GPO.User.ExtensionData.Name -contains "Folder Redirection") {
         $obj = [PSCustomObject]@{
